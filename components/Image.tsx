cfb12bbd8ff18400077ce07e4602e8e71db65754
pei <epep8413@gmail.com>
2026-09-10 17:10:10 +0800

    update mobile banner image width

--- components/Image.tsx
+++ components/Image.tsx
@@ -2,8 +2,14 @@ import NextImage, { ImageProps } from 'next/image'
 
 const basePath = process.env.BASE_PATH
 
-const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={`${basePath || ''}${src}`} {...rest} />
-)
+const Image = ({ src, ...rest }: ImageProps) => {
+  if (
+    (src && String(src).startsWith('https://img.peiwang.dev')) ||
+    String(src).startsWith('https://images.unsplash.com')
+  ) {
+    return <NextImage src={src} {...rest} />
+  }
+  return <NextImage src={`${basePath || ''}${src}`} {...rest} />
+}
 
 export default Image
--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -31,104 +31,106 @@ export default function PostLayout({ content, next, prev, children }: LayoutProp
   const displayImage = images && images.length > 0 ? images[0] : ''
 
   return (
-    <SectionContainer>
-      <ScrollTopAndComment />
+    <>
       {displayImage && (
-        <div className="w-full">
-          <div className="relative aspect-2/1 w-full">
-            <Image src={displayImage} alt={title} fill className="object-cover" />
-          </div>
+        <div className="relative -mx-4 aspect-2/1">
+          <Image src={displayImage} alt={title} fill className="object-cover" />
         </div>
       )}
-      <article>
-        <div>
-          <header className="border-b border-gray-200 pt-10 pb-8 dark:border-gray-700">
-            <div className="space-y-2">
-              <div>
-                <PageTitle>{title}</PageTitle>
-              </div>
-              <div className="text-sm leading-6 text-gray-500 dark:text-gray-400">
-                {summary && <p className="mb-3 text-base">{summary}</p>}
-                <time dateTime={date}>{format(parseISO(date), 'MMMM d, yyyy')}</time>
-                <span className="ml-4">read {Math.max(1, Math.ceil(readingTime.minutes))} min</span>
-              </div>
-            </div>
-          </header>
-          <div className="pb-8">
-            <div>
-              {/* blog 文章本體 */}
-              <div className="postContent">{children}</div>
-              <PostReaction slug={slug} />
-              <div className="py-6 text-sm text-gray-600 dark:text-gray-400">
-                <Link href={discussUrl(path)} rel="nofollow">
-                  Discuss on Twitter
-                </Link>
-                {` • `}
-                <Link href={editUrl(filePath)}>View on GitHub</Link>
-              </div>
-              {siteMetadata.comments && (
-                <div
-                  className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300"
-                  id="comment"
-                >
-                  <Comments slug={slug} />
+      <SectionContainer>
+        <ScrollTopAndComment />
+        <article>
+          <div>
+            <header className="border-b border-gray-200 pt-10 pb-8 dark:border-gray-700">
+              <div className="space-y-2">
+                <div>
+                  <PageTitle>{title}</PageTitle>
                 </div>
-              )}
-            </div>
-            <footer className="border-t border-gray-200 pt-6 dark:border-gray-700">
-              <div className="text-sm leading-6 font-medium">
-                {tags && (
-                  <div className="pb-8">
-                    <h2 className="mb-2 text-sm text-gray-500 dark:text-gray-400">Tags</h2>
-                    <div className="flex flex-wrap">
-                      {tags.map((tag) => (
-                        <Tag key={tag} text={tag} />
-                      ))}
-                    </div>
+                <div className="text-sm leading-6 text-gray-500 dark:text-gray-400">
+                  {summary && <p className="mb-3 text-base">{summary}</p>}
+                  <time dateTime={date}>{format(parseISO(date), 'MMMM d, yyyy')}</time>
+                  <span className="ml-4">
+                    read {Math.max(1, Math.ceil(readingTime.minutes))} min
+                  </span>
+                </div>
+              </div>
+            </header>
+            <div className="pb-8">
+              <div>
+                {/* blog 文章本體 */}
+                <div className="postContent">{children}</div>
+                <PostReaction slug={slug} />
+                <div className="py-6 text-sm text-gray-600 dark:text-gray-400">
+                  <Link href={discussUrl(path)} rel="nofollow">
+                    Discuss on Twitter
+                  </Link>
+                  {` • `}
+                  <Link href={editUrl(filePath)}>View on GitHub</Link>
+                </div>
+                {siteMetadata.comments && (
+                  <div
+                    className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300"
+                    id="comment"
+                  >
+                    <Comments slug={slug} />
                   </div>
                 )}
-                {(next || prev) && (
-                  <nav
-                    className="grid gap-8 border-t border-gray-200 py-8 sm:grid-cols-2 dark:border-gray-700"
-                    aria-label="文章導覽"
-                  >
-                    {prev && prev.path && prev.title && (
-                      <div className="articleNavItem col-start-1">
-                        <h2 className="mb-1 text-sm text-gray-500 dark:text-gray-400">
-                          Previous Article
-                        </h2>
-                        <div className="text-primary-600 hover:text-primary-800 dark:hover:text-primary-400 dark:text-gray-200">
-                          <Link href={`/${prev.path}`}>{prev.title}</Link>
-                        </div>
+              </div>
+              <footer className="border-t border-gray-200 pt-6 dark:border-gray-700">
+                <div className="text-sm leading-6 font-medium">
+                  {tags && (
+                    <div className="pb-8">
+                      <h2 className="mb-2 text-sm text-gray-500 dark:text-gray-400">Tags</h2>
+                      <div className="flex flex-wrap">
+                        {tags.map((tag) => (
+                          <Tag key={tag} text={tag} />
+                        ))}
                       </div>
-                    )}
+                    </div>
+                  )}
+                  {(next || prev) && (
+                    <nav
+                      className="grid gap-8 border-t border-gray-200 py-8 sm:grid-cols-2 dark:border-gray-700"
+                      aria-label="文章導覽"
+                    >
+                      {prev && prev.path && prev.title && (
+                        <div className="articleNavItem col-start-1">
+                          <h2 className="mb-1 text-sm text-gray-500 dark:text-gray-400">
+                            Previous Article
+                          </h2>
+                          <div className="text-primary-600 hover:text-primary-800 dark:hover:text-primary-400 dark:text-gray-200">
+                            <Link href={`/${prev.path}`}>{prev.title}</Link>
+                          </div>
+                        </div>
+                      )}
 
-                    {next && next.path && next.title && (
-                      <div className="articleNavItem col-start-2 justify-self-end sm:text-right">
-                        <h2 className="mb-1 text-sm text-gray-500 dark:text-gray-400">
-                          Next Article
-                        </h2>
-                        <div className="text-primary-600 hover:text-primary-800 dark:hover:text-primary-400 dark:text-gray-200">
-                          <Link href={`/${next.path}`}>{next.title}</Link>
+                      {next && next.path && next.title && (
+                        <div className="articleNavItem col-start-2 justify-self-end sm:text-right">
+                          <h2 className="mb-1 text-sm text-gray-500 dark:text-gray-400">
+                            Next Article
+                          </h2>
+                          <div className="text-primary-600 hover:text-primary-800 dark:hover:text-primary-400 dark:text-gray-200">
+                            <Link href={`/${next.path}`}>{next.title}</Link>
+                          </div>
                         </div>
-                      </div>
-                    )}
-                  </nav>
-                )}
-              </div>
-              <div>
-                <Link
-                  href={`/${basePath}`}
-                  className="text-primary-600 hover:text-primary-600 dark:text-primary-400 dark:hover:text-primary-300"
-                  aria-label="Back to the blog"
-                >
-                  &larr; Back to the blog
-                </Link>
-              </div>
-            </footer>
+                      )}
+                    </nav>
+                  )}
+                </div>
+                <div>
+                  <Link
+                    href={`/${basePath}`}
+                    className="text-primary-600 hover:text-primary-600 dark:text-primary-400 dark:hover:text-primary-300"
+                    aria-label="Back to the blog"
+                  >
+                    &larr; Back to the blog
+                  </Link>
+                </div>
+              </footer>
+            </div>
           </div>
-        </div>
-      </article>
-    </SectionContainer>
+        </article>
+      </SectionContainer>
+    </>
   )
 }
